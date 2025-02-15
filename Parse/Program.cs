d4215966d19c4cd239b7dbe244eccad46c2f5729
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-15 10:50:23 +0530

    understanding parese vs try parse

---
+++ Parse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ParseExample
+{
+    static void Main()
+    {
+        string validNumber = "123";
+        string invalidNumber = "ABC";
+        int parsedNumber;
+
+        if (int.TryParse(validNumber, out parsedNumber))
+        {
+            Console.WriteLine("TryParse Success: " + parsedNumber);
+        }
+        else
+        {
+            Console.WriteLine("TryParse Failed");
+        }
+
+        try
+        {
+            int result = int.Parse(invalidNumber);
+            Console.WriteLine("Parse Success: " + result);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Parse Failed: Invalid Format");
+        }
+    }
+}
