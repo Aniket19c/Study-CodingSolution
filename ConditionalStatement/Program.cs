1e694e95363d8dafd2ebcce64c33d380dc13250e
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-15 10:44:38 +0530

    conditional statement

---
+++ ConditionalStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ConditionalStatements
+{
+    static void Main()
+    {
+        int number = 10;
+        if (number > 0)
+        {
+            Console.WriteLine("Positive Number");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("Negative Number");
+        }
+        else
+        {
+            Console.WriteLine("Zero");
+        }
+    }
+}
