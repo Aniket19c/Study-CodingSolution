f34707ef3b2e9623e0f979ad55b4818c22aeeadd
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-15 10:58:23 +0530

    Check the number is prime Number or not

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class PrimeNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        bool isPrime = num > 1;
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            Console.WriteLine(num + " is a Prime Number.");
+        else
+            Console.WriteLine(num + " is not a Prime Number.");
+    }
+}
