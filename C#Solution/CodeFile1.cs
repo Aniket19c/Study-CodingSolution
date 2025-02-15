1884f5fc8e997c8bc83495949bc4c914d7a289ea
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-15 10:47:35 +0530

    using Loops

---
+++ C#Solution/CodeFile1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class LoopExamples
+{
+    static void Main()
+    {
+        Console.WriteLine("For Loop Example:");
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("While Loop Example:");
+        int count = 1;
+        while (count <= 5)
+        {
+            Console.WriteLine(count);
+            count++;
+        }
+
+        Console.WriteLine("Do-While Loop Example:");
+        int num = 1;
+        do
+        {
+            Console.WriteLine(num);
+            num++;
+        } while (num <= 5);
+    }
+}
